2b965be6d33da73c726dedca9b619da8edae1e9a
0RootLoL0 <0rootlol0@gmail.com>
2019-11-28 09:20:05 +0300

    it is concept

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
 import time
-from flask import Flask
+import json
+import sqlite3
+import secrets
+import datetime
+from flask import Flask, request, g
 app = Flask(__name__)
+
+def connect_db():
+    rv = sqlite3.connect("test_db_sqlite.db")
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+def isLesson(minStart, minEnd, inMin):
+    if minStart <= inMin and minEnd >= inMin:
+        return True
+    else:
+        return False
+
+def getNumLesson(tables, min):
+    Lesson = 0
+    if min < tables[0]["StartMinL"]:
+        return Lesson
+    elif min > tables[len(tables)-1]["StartMinL"]:
+        return len(tables)+1
+
+    for table in tables:
+        if isLesson(table["StartMinL"], table["EndMinL"] , min):
+            break
+        else:
+            Lesson += 1
+
+    return Lesson
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/')
 def hello_world():
@@ -9,6 +49,28 @@ def hello_world():
 @app.route('/getTime')
 def getTime():
     return str(int(time.time()))
+
+@app.route('/getLesson')
+def getLesson():
+    token = request.args.get("token")
+    if token == "":
+        return "{\"status\": \"hui\"}"
+    
+    mess = ""
+    db = get_db()
+    cur = db.execute('SELECT * FROM "main"."users_rootlolhui" WHERE "token_clock"=\''+token+'\';')
+    entries = cur.fetchall()
+    if len(entries) == 1:
+        now = datetime.datetime.now()
+        Lesson = getNumLesson(json.loads(entries[0]["schedule_calls"]), now.hour*60+now.minute)
+        print(Lesson)
+        if Lesson == 0:
+            mess = json.dumps({"Lesson": "не начились"})
+        elif Lesson == 8:
+            mess = json.dumps({"Lesson": "кончились"})
+        else:
+            mess = json.loads(entries[0]["lessons_monday"])[Lesson]["Lesson"]
+    return mess
 
 if __name__ == '__main__':
     app.run()
--- testFun.py
+++ testFun.py
@@ -1,20 +1,32 @@
-def getNumLesson(tadle, hour, min):
-    if tadle[0]["stLessHour"] >= hour and tadle[0]["stLessMin"] > min:
-        return 0
-    elif hour >= tadle[0]["stLessHour"] and min >= tadle[0]["stLessMin"] and hour <= tadle[0]["endLessHour"]:
-        if hour == tadle[0]["endLessHour"] and min <= tadle[0]["endLessMin"]:
-            return 1
-    return 8
+import datetime
+def isLesson(minStart, minEnd, inMin):
+    if minStart <= inMin and minEnd >= inMin:
+        return True
+    else:
+        return False
 
+def getNumLesson(tables, min):
+    Lesson = 0
+    if min < tables[0]["StartMinL"]:
+        return Lesson
 
+    for table in tables:
+        if isLesson(table["StartMinL"], table["EndMinL"] , min):
+            Lesson += 1
+            break
+        else:
+            Lesson += 1
+
+    return Lesson
+          
 tables = [
-  {"stLessHour": 8, "stLessMin": 30, "endLessHour": 9, "endLessMin": 15},
-  {"stLessHour": 9, "stLessMin": 30, "endLessHour": 10, "endLessMin": 15},
-  {"stLessHour": 10, "stLessMin": 30, "endLessHour": 11, "endLessMin": 15},
-  {"stLessHour": 11, "stLessMin": 30, "endLessHour": 12, "endLessMin": 15},
-  {"stLessHour": 12, "stLessMin": 30, "endLessHour": 13, "endLessMin": 15},
-  {"stLessHour": 13, "stLessMin": 30, "endLessHour": 14, "endLessMin": 15},
-  {"stLessHour": 14, "stLessMin": 30, "endLessHour": 15, "endLessMin": 15}
+    {"StartMinL": 510, "EndMinL": 555},
+    {"StartMinL": 570, "EndMinL": 615},
+    {"StartMinL": 510, "EndMinL": 555},
+    {"StartMinL": 510, "EndMinL": 555},
+    {"StartMinL": 510, "EndMinL": 555},
+    {"StartMinL": 510, "EndMinL": 555},
+    {"StartMinL": 510, "EndMinL": 555},
 ]
-
-print(getNumLesson(tables, 9, 10))
+now = datetime.datetime.now()
+print(getNumLesson(tables, now.hour*60+now.minute))
